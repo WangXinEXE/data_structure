cdc22cf528ea6e982f0b4c975da685de1211c60f
wangXin <wangxin@qq.com>
2021-04-29 10:12:25 +0800

    4.29 update

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -6,13 +6,21 @@ public class LinkedList {
         HeroNode hero2 = new HeroNode(2,"卢俊义","玉麒麟");
         HeroNode hero3 = new HeroNode(3,"吴用","智多星");
         HeroNode hero4 = new HeroNode(4,"李逵","黑旋风");
+        HeroNode hero5 = new HeroNode(4,"wuyifan","wuyifan");
 
         SingleLinkedList list = new SingleLinkedList();
-        list.add(hero1);
-        list.add(hero2);
-        list.add(hero3);
-        list.add(hero4);
-        list.list();
+//        list.add(hero1);
+//        list.add(hero2);
+//        list.add(hero3);
+//        list.add(hero4);
+
+
+          list.addByOrder(hero4);
+          list.addByOrder(hero2);
+          list.addByOrder(hero1);
+          list.addByOrder(hero3);
+          //list.addByOrder(hero5);
+          list.list();
     }
 
 }
@@ -37,6 +45,33 @@ class SingleLinkedList {
         //当退出循环时,说明已经找到了链表的最后.
         temp.next = heroNode;  //指向新的节点.
     }
+
+    public void addByOrder(HeroNode heroNode) {
+        //因为是单链表,找到的temp是位于添加前的一个位置,否则插入不了
+        HeroNode temp = head;  //辅助变量
+        boolean flag = false;  //提示添加的节点是否存在,默认false
+
+        while (true) {
+            if(temp.next == null) {
+                break;   //说明已经找到链表的最后
+            }
+            if(temp.next.no > heroNode.no) {  //说明节点位置已经找到
+                break;
+            }
+            if(temp.next.no == heroNode.no) {     //说明位置已经存在
+                flag = true;
+                break;
+            }
+            temp = temp.next; //后移,遍历链表
+        }
+        if(flag) {
+            System.out.printf("当前要添加位置已经存在");
+        }else {
+            heroNode.next = temp.next;   //向当前位置添加数据
+            temp.next = heroNode;
+        }
+    }
+
     //遍历链表的方法
     public void list() {
         if(head.next == null) {
@@ -50,12 +85,9 @@ class SingleLinkedList {
             }
             System.out.println(temp);
             temp = temp.next;
-
         }
     }
 }
-
-
 //英雄节点的信息,每一个Node代表一个节点
 class HeroNode {
     public int no;  //编号
@@ -69,7 +101,6 @@ class HeroNode {
         this.nickname = hNickname;
     }
     //为了显示方便重写toString
-
 
     @Override
     public String toString() {
