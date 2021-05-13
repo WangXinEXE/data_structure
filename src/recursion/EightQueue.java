8c560cd4dd79118dfde8df0790a90f51f0310734
wangXin <1627297962@qq.com>
2021-05-13 23:32:06 +0800

    5.12 update递归 八皇后问题

---
+++ src/recursion/EightQueue.java
@@ -0,0 +1,54 @@
+package recursion;
+//八皇后问题
+public class EightQueue {
+    //定义一个有8个皇后
+    int max = 8;
+    int count = 0;
+    //存放结果的数组
+    int[] array = new int[max];
+    public static void main(String[] args) {
+        new EightQueue().check(0);
+
+    }
+
+    public void check(int n) {
+        if(n == max) {
+            getResult();
+            return;
+        }
+        for (int i = 0;i < max;i++) {
+            //先把皇后n放到改行的第一列
+            array[n] = i;
+            //判断将皇后n放到第i列时,是否冲突
+            if(judge(n)) { //如果不冲突
+                //接着放第n + 1个皇后
+                check(n + 1);
+            }
+            //如果冲突,就继续执行array[n] = i,即将此皇后在本行后移一个位置
+        }
+
+    }
+
+
+
+
+    //判断当前皇后是否和之前的摆放冲突
+    public boolean judge(int n) {
+        for (int i = 0;i < n;i++) {
+            if(array[i] == array[n] || Math.abs(i - n) == Math.abs(array[i] - array[n])) {  //如果在同一列或同一斜线上则return false
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void getResult() {
+        count++;
+        for (int i = 0;i < array.length;i++) {
+            System.out.printf(array[i] + " ");
+        }
+        System.out.println(" " + count);
+    }
+
+}
