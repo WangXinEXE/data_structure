6c49e1c4b761ec29741f7c98908707dbcf4235ab
wangXin <1627297962@qq.com>
2021-05-14 23:46:55 +0800

    5.12 update排序算法

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -4,14 +4,18 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {4396,-5,7,20,3,16};
         //第一趟排序最大的就在最后
-        for(int i = 0;i < arr.length - 1;i++) {
-            int temp;
-            if(arr[i] > arr[i + 1]) {
-                temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+        int temp;
+        for(int k = 0;k < arr.length - 1;k++) {
+            for(int i = 0;i < arr.length - k - 1;i++) {
+                if(arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
             }
+
         }
+
         for (int j = 0;j < arr.length;j++) {
             System.out.println(arr[j]);
         }
