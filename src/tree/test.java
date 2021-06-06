28de68f85af9fadfac0a07fc82fc2aa68a829320
wangXin <1627297962@qq.com>
2021-06-06 20:13:54 +0800

    6.6 update HuffmanTree

--- src/tree/test.java
+++ src/tree/test.java
@@ -2,8 +2,11 @@ package tree;
 
 public class test {
     public static void main(String[] args) {
-        int arr[] = {4396,454};
+        int arr[] = {4396, 454};
 
+        for (int i = 0; i < arr.length; i++) {
+
+        }
 
     }
 }
