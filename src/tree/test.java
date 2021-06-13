bbc993a8f9f525eb187e6af8028d2bd87a394de0
wangXin <1627297962@qq.com>
2021-06-13 22:54:02 +0800

    6.13 update

--- src/tree/test.java
+++ src/tree/test.java
@@ -40,11 +40,6 @@ public class test {
             System.out.println(arr[i]);
             System.out.println(arr[i]);
             System.out.println(arr[i]);
-        }for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-            System.out.println(arr[i]);
-            System.out.println(arr[i]);
-            System.out.println(arr[i]);
         }
 
     }
