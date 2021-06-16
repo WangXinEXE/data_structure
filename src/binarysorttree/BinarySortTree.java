8479cc0c49f44d53e5f0b6ef501e60bd9b5011c3
wangXin <1627297962@qq.com>
2021-06-16 22:48:40 +0800

    6.16 BinarySortTree update

--- src/binarysorttree/BinarySortTree.java
+++ src/binarysorttree/BinarySortTree.java
@@ -2,15 +2,21 @@ package binarysorttree;
 
 public class BinarySortTree {
     public static void main(String[] args) {
-
+        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        BinarySortTrees binarySortTrees = new BinarySortTrees();
+        for (int i : arr) {
+            binarySortTrees.add(new Node(i));
+        }
+        binarySortTrees.infixOrder();
     }
 
 }
-class BinarySortTrees{
+
+class BinarySortTrees {
     private Node root; // 根节点
 
     public void add(Node node) {
-        if(root == null) {
+        if (root == null) {
             root = node;
         } else {
             root.add(node);
@@ -18,14 +24,13 @@ class BinarySortTrees{
     }
 
     public void infixOrder() {
-        if(root != null) {
+        if (root != null) {
             root.infixOrder();
         } else {
             System.out.println("这棵树是空的");
         }
     }
 }
-
 
 
 class Node {
@@ -46,31 +51,32 @@ class Node {
 
     //递归添加节点的方法,满足二叉排序树的要求
     public void add(Node node) {
-        if(node == null) {
+        if (node == null) {
             return;
         }
-        if(node.value < this.value) {
-            if(this.left == null) {
+        if (node.value < this.value) {
+            if (this.left == null) {
                 this.left = node;
             } else {
                 //递归的向左子树添加
                 this.left.add(node);
             }
         } else {
-            if(this.right == null) {
+            if (this.right == null) {
                 this.right = node;
             } else {
                 this.right.add(node);
             }
         }
     }
+
     //中序遍历
     public void infixOrder() {
-        if(this.left != null) {
+        if (this.left != null) {
             this.left.infixOrder();
         }
         System.out.println(this);
-        if(this.right != null) {
+        if (this.right != null) {
             this.right.infixOrder();
         }
     }
