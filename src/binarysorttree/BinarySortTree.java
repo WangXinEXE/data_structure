1ab413579945dbc309b7da4cb3bfc4782cb6bc75
wangXin <1627297962@qq.com>
2021-06-16 22:24:20 +0800

    6.16 BinarySortTree update

--- src/binarysorttree/BinarySortTree.java
+++ src/binarysorttree/BinarySortTree.java
@@ -6,3 +6,18 @@ public class BinarySortTree {
     }
 
 }
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    //递归添加节点的方法,满足二叉排序树的要求
+    public void add(Node node) {
+
+    }
+}
