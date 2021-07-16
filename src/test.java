7a3d57c7a82234c617f1dd395ffdb78e824d698d
wangXin <1627297962@qq.com>
2021-07-16 15:18:42 +0800

    7.14 AVLTree.java

--- src/test.java
+++ src/test.java
@@ -3,9 +3,7 @@ public class test {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+
 
     }
 
